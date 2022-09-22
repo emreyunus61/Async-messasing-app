8da3b13d36bc54926d6dfa99c4b272973e517cc3
Yunus Emre ÖZTÜRK <50083324+emreyunus61@users.noreply.github.com>
2022-09-22 23:05:17 +0300

    Project created

---
+++ src/main/java/com/sixtyone/asyncmessasingapp/AsyncMessasingAppApplication.java
@@ -0,0 +1,13 @@
+package com.sixtyone.asyncmessasingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncMessasingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncMessasingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sixtyone/asyncmessasingapp/AsyncMessasingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixtyone.asyncmessasingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncMessasingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
